b841b665817b406fa446bf691ef8a2e2e4f7558f
Caterina Greco <caterinagreco@MacBook-Pro-di-Caterina.local>
2021-04-04 12:13:45 +0200

    fixed example on mac and added example midi

--- example.py
+++ example.py
@@ -1,20 +1,22 @@
 import pyo
 from mido import MidiFile
 import random
+import platform
 
 
 def main():
-    s = pyo.Server().boot().start()
+    audio = 'coreaudio' if platform.system() == 'Darwin' else 'portaudio'
+    s = pyo.Server(audio=audio).boot().start()
 
     # A little audio synth to play the MIDI events.
     mid = pyo.Notein()
     amp = pyo.MidiAdsr(mid["velocity"])
     pit = pyo.MToF(mid["pitch"])
-    osc = pyo.Osc(pyo.SquareTable(), freq=pit, mul=amp).mix(1)
+    osc = pyo.Osc(pyo.ParaTable(), freq=pit, mul=amp).mix(1)
     rev = pyo.STRev(osc, revtime=1, cutoff=4000, bal=0.2).out()
 
     # Opening the MIDI file...
-    mid = MidiFile("/Users/lucabutera/Downloads/Titantic.mid")
+    mid = MidiFile("midi/two_channels.mid")
 
     # ... and reading its content.
     for i, message in enumerate(mid.play()):
@@ -23,7 +25,7 @@ def main():
         # method. This method programmatically adds a MIDI message to the
         # server's internal MIDI event buffer.
         if i % 100 == 0 or i == 0:
-            playing = random.sample([0, 1, 2, 3], random.randint(1, 4))
+            playing = random.sample([0, 1], random.randint(1, 2))
             print(playing)
         if message.channel in playing or not message.type == 'note_on':
             s.addMidiEvent(*message.bytes())
