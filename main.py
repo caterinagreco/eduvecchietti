ac636292acb4af3d72b9ca1530e269889cef0170
Caterina Greco <caterinagreco@MacBook-Pro-di-Caterina.local>
2021-05-02 13:07:37 +0200

    Aggiornamento configurazione

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import json
+from argparse import ArgumentParser
 
 from game.game import Game
 
 if __name__ == "__main__":
-    config_path = 'config.mac.json'
-    with open(config_path, 'r') as f:
+    parser = ArgumentParser(description='Play the game with no name.')
+    parser.add_argument('config', type=str, help='Path to the config file.', required=True)
+    args = parser.parse_args()
+    with open(args.config, 'r') as f:
         config = json.load(f)
     game = Game(config=config)
     game.configure()
-    game.player.active_channels = game.player.get_existing_channels()
     game.start()
