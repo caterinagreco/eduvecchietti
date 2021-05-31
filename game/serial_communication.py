0a094d3b51efdecb0cde6802716821f6c4230be9
Caterina Greco <caterinagreco@MacBook-Pro-di-Caterina.local>
2021-05-31 10:48:46 +0200

    Added hendling of serial port retry

--- game/serial_communication.py
+++ game/serial_communication.py
@@ -4,7 +4,17 @@ from serial import Serial
 class SerialCommunication:
 
     def __init__(self):
-        self.ser = Serial('/dev/ttyACM0', 9600, timeout=1)
+        port = 0
+        ser = None
+        while ser is None:
+            try:
+                ser = Serial(f'/dev/ttyACM{port}', 9600, timeout=1)
+            except FileNotFoundError as e:
+                if port < 10:
+                    port += 1
+                else:
+                    raise e
+        self.ser = ser
         self.ser.flush()
 
     def led_do(self, instrument: str, state: bool, color: str):
